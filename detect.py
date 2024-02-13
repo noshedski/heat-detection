18e4310eaeaaa4e4fda4e9b517d310f111a24b7c
Noah Turpin <noahhturpinn@gmail.com>
2024-02-13 10:09:51 -0500

    Changed thresholding values for grayscale

--- detect.py
+++ detect.py
@@ -9,7 +9,7 @@ def find_organism(arr):
     blurred = cv2.GaussianBlur(image, (5, 5), 0)
     
     #Apply thresholding to identify potential organisms
-    _, thresholded = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)    
+    _, thresholded = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)    
 
     # Find contours in the thresholded image, the contours that are found are potential organisms
     contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
